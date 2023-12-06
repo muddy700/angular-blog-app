35a9ac4121a9f71134bc6576d04952379b8037f6
Mohamed Mfaume Mohamed <mohamed.mohamed5@tra.go.tz>
2023-12-06 22:34:50 +0300

    feat(Home-Page): Allow user to filter posts by category

--- src/app/home/view/view.component.ts
+++ src/app/home/view/view.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { CategoriesService } from 'src/app/shared/services/categories.service';
 import { Category } from 'src/app/shared/entities/category';
 import { HelpersService } from 'src/app/shared/services/helpers.service';
@@ -11,9 +11,13 @@ import { PostsService } from 'src/app/shared/services/posts.service';
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css'],
 })
-export class ViewComponent implements OnInit {
+export class ViewComponent implements OnInit, OnDestroy {
+  activeCategoryId: string = '';
+  subscriptions: Subscription[] = [];
   allCategories$: Observable<Category[]>;
-  allPosts$: Observable<Post[]>;
+
+  allPosts: Post[] = [];
+  isFetchingPosts$: Observable<Boolean>;
   isFetchingCategories$: Observable<Boolean>;
 
   constructor(
@@ -21,13 +25,35 @@ export class ViewComponent implements OnInit {
     public helpersService: HelpersService,
     public postsService: PostsService
   ) {
+    this.isFetchingPosts$ = this.postsService.loading$;
     this.allCategories$ = this.categoriesService.entities$;
-    this.allPosts$ = this.postsService.entities$;
     this.isFetchingCategories$ = this.categoriesService.loading$;
   }
 
   ngOnInit(): void {
+    this.fetchPosts();
     this.categoriesService.getAll();
-    this.postsService.getWithQuery('');
+  }
+
+  fetchPosts(): void {
+    let request: Subscription = this.postsService
+      .getWithQuery('')
+      .subscribe((posts: Post[]) => (this.allPosts = posts));
+
+    this.subscriptions.push(request);
+  }
+
+  filterPostsByCategory(categoryId: string): void {
+    this.activeCategoryId = categoryId;
+
+    let request: Subscription = this.postsService
+      .getWithQuery('&filters[category][id][$eq]=' + categoryId)
+      .subscribe((posts: Post[]) => (this.allPosts = posts));
+
+    this.subscriptions.push(request);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((req: Subscription) => req.unsubscribe);
   }
 }
